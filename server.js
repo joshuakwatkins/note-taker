c07033caa957431f056bb871eb253f777962c31d
Josh Watkins <joshwatkinssfs@gmail.com>
2021-10-14 23:54:12 -0400

    still attempting heroku fixes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const api = require('./routes/index')
 const path = require('path')
 
-const PORT = process.env.PORTgit  || 3001;
+const PORT = process.env.PORT  || 3001;
 
 const app = express()
 
